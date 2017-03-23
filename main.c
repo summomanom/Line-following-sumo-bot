982378655b2a4906416eaa4dc4bfc3d7bd5fe3b0
summomanom <shawn.l.oconnell@gmail.com>
2017-03-23 10:16:53 -0700

    Revert "Revert "90 degree turns rev1""
    This reverts commit e485cda7ff3618535dc0e88aff6cd35cfc515682.

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include "sumovore.h"
 #include "motor_control.h"
 #include "interrupts.h"
+#include "osc.h"
 
-
+#define _XTAL_FREQ 32000000
+char i = 0;
 // main acts as a cyclical task sequencer
 void main(void)
 {
@@ -14,10 +16,10 @@ void main(void)
                       // IO pins, the ADC, the 
                       // USART and the default
                       // threshold
-    printf("\n\rKwantlen APSC1299 simple curve follower -- with error codes\n\r"); 
+   
     ClrWdt();         // defined in <p18f4525.h>
 
-threshold = 280u; // to change from default value
+threshold = 285u; // to change from default value
                      // uncomment and change to any unsigned int <1024u -- most usually <512u
 
     while(1)
@@ -27,7 +29,9 @@ threshold = 280u; // to change from default value
 	                    // each LED indicates a sensor
 	                    // value. If you need to use the LED's for
 	                    // a different purpose change this line
-	                    // and make your own LED setting function
+        
+            
+        // and make your own LED setting function
         motor_control();    // function from motor_control.c 
         ClrWdt();           // defined in <p18f4525.h>
         if(lvd_flag_set())  LVtrap();
