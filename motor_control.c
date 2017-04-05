859b1e91af60f160a570491b0de487e792f0ecc6
freyaor <freya.obersatrose@email.kpu.ca>
2017-04-04 20:33:28 -0700

    Update motor_control.c

--- motor_control.c
+++ motor_control.c
@@ -30,10 +30,13 @@
     int medium_left_wheel=17;
     int slow_left_wheel=21;
     
-    int wait_for_8cm = 10000;  //for half battery
+    //int wait_for_8cm = 10000;  //for half battery
+    int wait_for_8cm = 0;  //for low battery
+    
    
-    //int wait_for_8cm = 45536;  // for full battery    
-    int turn_around_time =9286; // for half battery    
+    //int wait_for_8cm = 45536;  // for full battery   
+    int turn_around_time =0;  // for almost dead battery
+    //int turn_around_time =9286; // for half battery    
     //int turn_around_time = 15286; // for full battery
 
     void motor_control(void)
@@ -84,8 +87,7 @@
             case 0b00000u:
             {
                 //small timer to make sure it is a gap
-               ensure_whitespace();
-               //move forward 8cm
+               ensure_whitespace();               //move forward 8cm
                if (SeeLine.B == 0b00000u)
                {
                OpenTimer0(TIMER_INT_OFF & T0_SOURCE_INT & T0_16BIT & T0_PS_1_32);
@@ -107,29 +109,77 @@
                  TMR0IF = 0;
 
                  WriteTimer0(turn_around_time);
-                 while(TMR0IF == 0&&SeeLine.B == 0b00000u)
+
+
+                 while(TMR0IF == 0&&SeeLine.B==0b00000u)
+
                  {
                    spin_right_fast();  
                    check_sensors();
-                     set_leds();
+                   set_leds();
+                     
                  }
                  
                  CloseTimer0();
                }
+               else if (SeeLine.B == 0b10000u)
+               {
+                   while(SeeLine.B != 0b00100u&&SeeLine.B!=0b01100u&&SeeLine.B!=0b00110u)
+                   {
+
+                       straight_fwd_fast();
+                       check_sensors();
+                       set_leds();
+                   }
+                   if(SeeLine.B !=0b00000u)
+                   {
+                       straight_fwd_fast();
+                       check_sensors();
+                       set_leds();
+                   }
+                   if(SeeLine.B == 0b00000u)
+                   {
+                       turn_right2centre();
+                       check_sensors();
+                       set_leds();
+                   }
+               }
+               
+               else if (SeeLine.B == 0b00001u)
+               {
+
+                   while(SeeLine.B != 0b00100u&&SeeLine.B!=0b01100u&&SeeLine.B!=0b00110u)
+                   {
+
+                           
+                       straight_fwd_fast();
+                       check_sensors();
+                       set_leds();
+                   }
+                   if(SeeLine.B != 0b00000u)
+                   {
+                       straight_fwd_fast();
+                       check_sensors();
+                       set_leds();
+                       
+                   }
+                   if(SeeLine.B == 0b00000u)
+                   {
+                       turn_left2centre();
+                       check_sensors();
+                       set_leds();
+                   }
+               }
                
             }
-
+            break;
                            
-               
-
-                           
-                           break;
             case 0b11111u:
             {
                  OpenTimer0(TIMER_INT_OFF & T0_SOURCE_INT & T0_16BIT & T0_PS_1_8);
                 TMR0IF = 0;
                  WriteTimer0(0);
-                 while(TMR0IF == 0)
+                 while(TMR0IF == 0&&SeeLine.B != 0b00000u)
                  {
                    follow_simple_curves(); 
                    check_sensors();
@@ -198,12 +248,10 @@
                     }
                     while(1)
                      motors_brake_all();
-                }
-                
-                
-                 
+                }  
             }
             break;
+            
             default:   
                 follow_simple_curves();
                 break;
