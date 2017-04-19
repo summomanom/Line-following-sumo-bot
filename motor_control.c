222e3f4fdc72d15fb4f384ae6ea62212546a2122
summomanom <shawn.l.oconnell@gmail.com>
2017-04-19 16:16:14 -0700

    Update motor_control.c

--- motor_control.c
+++ motor_control.c
@@ -21,18 +21,18 @@
     void spin_right_medium(void);
     void spin_right_slow(void);
     
-    //variables for speed modifier
+   //variables for speed modifier of the motors to allow for straighter movement
     int fast_right_wheel=-37;
     int medium_left_wheel=17;
     int slow_left_wheel=21;
 
     void motor_control(void)
     {
-         // very simple motor control
+         // Check the values of all the sensors to determine what the robot should be doing.
          switch(SeeLine.B)
          {
 
-             //cases for tight angle turns to the right 
+             //if the edge and a center led light up we begin testing to see if it is a tight angle.
              case 0b00101u:
              case 0b01001u:
              {
@@ -98,13 +98,13 @@
                      
                  }
              }
-                 break; 
-             
+                 break;
+    //If multiple sensors on the right side are detected move into these cases for right hand turn
              case 0b00111u:
              case 0b01111u:
              case 0b00011u:
    
-             {  
+             {  //recheck the sensors and then while the right side of sensors are lit up continue to move forward
                  check_sensors();    
                  set_leds(); 
                  while(SeeLine.B == 0b00111u || SeeLine.B == 0b01111u || SeeLine.B == 0b00011u)
@@ -113,7 +113,7 @@
                      check_sensors();    
                      set_leds(); 
                  }
-                 
+                 //If the sensors stop being all lit up on the right enter this while loop to spin to the right until we see a sensor combination that works for follow simple curve
                      while(SeeLine.B == 0b00000u || SeeLine.B ==0b00001u||SeeLine.B == 0b00100u||SeeLine.B == 0b00110u||SeeLine.B == 0b00011u)
                      {
                          
@@ -125,11 +125,11 @@
                  
                
             break;
-            //left 90 degrees
+            //If multiple sensors on the left side are detected move into these cases for right hand turns
              case 0b11100u:
              case 0b11000u:
              case 0b11110u:
-             { 
+             { //recheck the sensors and then while the left side of sensors are lit up continue to move forward
                 check_sensors();    
                 set_leds(); 
                  while(SeeLine.B == 0b11100u || SeeLine.B == 0b11110u || SeeLine.B == 0b11000u)
@@ -138,7 +138,7 @@
                      check_sensors();    
                      set_leds(); 
                  }
-                 
+                  //If the sensors stop being all lit up on the left enter this while loop to spin to the left until we see a sensor combination that works for follow simple curves
                      while(SeeLine.B == 0b00000u || SeeLine.B ==0b10000u||SeeLine.B == 0b00100u||SeeLine.B == 0b01100u||SeeLine.B == 0b11000u)
                          
                      {
@@ -150,6 +150,7 @@
              }
                  break;
             
+       //if a single led lights up use the follow simple curves code provided by coombes   
             case 0b00100u:
             case 0b00010u:
             case 0b01000u:
@@ -160,13 +161,14 @@
                            break;
 
 
+  //if no leds are lit up brake all the motors.
             case 0b00000u:
                             motors_brake_all();
                            break;
             default:       break;
           } 
     }
-
+//Code provided by Coombes. Adjusts the robot motors to operate at different speeds so it can adjust on a slightly curved path
     void follow_simple_curves(void)
     {
          if ( SeeLine.b.Center ) straight_fwd_medium();
