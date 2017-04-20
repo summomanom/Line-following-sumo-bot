003228b1fb7e4ebc1924f1f69909d1939841a8b0
summomanom <shawn.l.oconnell@gmail.com>
2017-04-19 17:03:36 -0700

    Update motor_control.c

--- motor_control.c
+++ motor_control.c
@@ -23,33 +23,36 @@
     void check_for_whitespace(char prescaler, int write_time);
     void ensure_whitespace(void);
             
-     //variables for speed modifier
+   //variables for speed modifier of the motors to allow for straighter movement
     int fast_right_wheel=-37;
     int medium_left_wheel=17;
     int slow_left_wheel=21;
     
     int wait_for_8cm = 10000;  //for half battery
-   
     //int wait_for_8cm = 45536;  // for full battery    
     int turn_around_time =9286; // for half battery    
     //int turn_around_time = 15286; // for full battery
 
     void motor_control(void)
     {
-         // very simple motor control
+        // Check the values of all the sensors to determine what the robot should be doing.
          switch(SeeLine.B)
          {
-               //if one side gets triggered continue forward. if white space is found turn tell centered on a line
+          //If multiple sensors on the left side are detected move into these cases for right hand turns
              case 0b11000u:
              case 0b11100u:
+         //Call check for whitespace to open up a small timer to allow the robot to cross an intersection. if the robot sees all white it will break out of the timer.
                  check_for_whitespace(32, 0);
+          //if the robot is on all white after the timer ends we know we passed a turn and must turn nutil centered on a line
                  if(SeeLine.B == 0b00000u)turn_left2centre();
                  break;
              
                  //if one side gets triggered continue forward. if white space is found turn tell centered on a line
              case 0b00011u:
              case 0b00111u:
-                 check_for_whitespace(32, 0);   
+               //Call check for whitespace to open up a small timer to allow the robot to cross an intersection. if the robot sees all white it will break out of the timer.
+                 check_for_whitespace(32, 0); 
+              //if the robot is on all white after the timer ends we know we passed a turn and must turn nutil centered on a line
                  if(SeeLine.B == 0b00000u)turn_right2centre();
                  break;
                  
@@ -57,8 +60,9 @@
              case 0b00101u:
              case 0b01001u:
              case 0b01101u:
+               //Call check for whitespace to open up a small timer to allow the robot to cross an intersection. if the robot sees all white it will break out of the timer.
                  check_for_whitespace(32, 0);   
-                
+                //if the robot is on all white after the timer ends we know we passed a turn and must turn nutil centered on a line
                  if(SeeLine.B == 0b00000u)turn_right2centre();
                  break;
              
@@ -66,7 +70,9 @@
              case 0b10100u:
              case 0b10110u:
              case 0b10010u:
+            //Call check for whitespace to open up a small timer to allow the robot to cross an intersection. if the robot sees all white it will break out of the timer.
                  check_for_whitespace(32, 0);   
+            //if the robot is on all white after the timer ends we know we passed a turn and must turn nutil centered on a line
                  if(SeeLine.B == 0b00000u)turn_left2centre();
                  break;
            
@@ -78,7 +84,7 @@
                            //no breaks all above readings end up here
                            follow_simple_curves();
                            break;
-                 
+    //if the robot detects all white we need to check if there is a gap or a need to turn around          
             case 0b00000u:
             {
                 //small timer to make sure it is a gap
@@ -116,14 +122,11 @@
                }
                
             }
-
-                           
-               
-
-                           
                            break;
+             //if we detect all blak we need to check if it is an intersection or if it is a landing pad
             case 0b11111u:
             {
+                //open a small timer to force the robot to follow curves 
                  OpenTimer0(TIMER_INT_OFF & T0_SOURCE_INT & T0_16BIT & T0_PS_1_8);
                 TMR0IF = 0;
                  WriteTimer0(0);
@@ -134,6 +137,7 @@
                      set_leds();
                 }
                 ensure_whitespace();
+                //after the short timer if the robot still ses all black we must stop on the landing pad 
                 if (SeeLine.B == 0b11111u)
                      motors_brake_all();
                  
@@ -145,7 +149,7 @@
 
           } 
     }
-    
+    //function that can be called wth differnt scalers and write time values that causes the robot to move forward until a certain time has passed or all white is deteccted.
     void check_for_whitespace(char prescaler, int write_time)
     {
         switch(prescaler)
@@ -262,7 +266,7 @@
         }
     } 
     
-
+//if the sensors go blamk we need to move forward to ensure that it is actually whitespace and not our sensors mising the line for a second
     void ensure_whitespace(void)
     {
         OpenTimer0(TIMER_INT_OFF & T0_SOURCE_INT & T0_16BIT & T0_PS_1_8);
@@ -276,6 +280,7 @@
                  }
     }        
 
+//updated the follow curves code provided by coombes to allow for more adjustments along the path. to stop the robot from going too far in one direction
     void follow_simple_curves(void)
     {
          if ( SeeLine.B == 0b00100u || SeeLine.B == 0b10010u || SeeLine.B == 0b01001u) straight_fwd_fast();
@@ -308,7 +313,6 @@
                  }
     }
 
-    //Spins in one spot to the left
     void spin_left_fast(void)
     {
       set_motor_speed(left, rev_fast, 0); 
@@ -327,7 +331,6 @@
       set_motor_speed(right, slow, 0); 
     }    
 
-    ///turns on one wheel to the left
     void turn_left_fast(void)
     {
       set_motor_speed(left, slow, 0); 
@@ -346,7 +349,6 @@
       set_motor_speed(right, medium, 0); 
     }
 
-    //move in a straight line
     void straight_fwd_fast(void)
 
     {
@@ -366,7 +368,6 @@
       set_motor_speed(right, slow, 0); 
     }
 
-    //spin in one place to the right
     void spin_right_fast(void)
     {
       set_motor_speed(left, fast, 0); 
@@ -385,7 +386,6 @@
       set_motor_speed(right, rev_slow, 0); 
     }
 
-    //spin on one wheel to the right
     void turn_right_fast(void)
     {
       set_motor_speed(left, fast, 0); 
